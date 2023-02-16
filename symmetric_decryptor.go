225ede93ae5b3babbf0de7472de7b5186de5128b
Roman Herasymenko <roman.herasymenko@gmail.com>
2023-02-16 17:38:01 +0100

    Adding implementations for symmetric decryptor and encryptor.

---
+++ symmetric_decryptor.go
@@ -0,0 +1,60 @@
+package fileencryptor
+
+import (
+	"encoding/binary"
+	"io"
+
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+type SymmetricDecryptor struct {
+	in        io.Reader
+	out       io.Writer
+	blockSize int
+	key       [32]byte
+}
+
+func NewSymmetricDecryptor(in io.Reader, out io.Writer, blockSize int, key [32]byte) (*SymmetricDecryptor, error) {
+	return &SymmetricDecryptor{
+		in:        in,
+		out:       out,
+		blockSize: blockSize,
+		key:       key,
+	}, nil
+}
+
+func (f *SymmetricDecryptor) Decrypt() error {
+	aead, err := chacha20poly1305.New(f.key[:])
+	if err != nil {
+		return err
+	}
+
+	var nonce [12]byte
+	if err := binary.Read(f.in, binary.LittleEndian, &nonce); err != nil {
+		return err
+	}
+
+	buf := make([]byte, f.blockSize+aead.Overhead())
+	for {
+		n, err := f.in.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		plaintext, err := aead.Open(nil, nonce[:], buf[:n], nil)
+		if err != nil {
+			return err
+		}
+
+		if _, err := f.out.Write(plaintext); err != nil {
+			return err
+		}
+
+		binary.LittleEndian.PutUint64(nonce[4:], binary.LittleEndian.Uint64(nonce[4:])+1)
+	}
+
+	return nil
+}
---
+++ symmetric_encryptor.go
@@ -0,0 +1,68 @@
+package fileencryptor
+
+import (
+	"crypto/rand"
+	"encoding/binary"
+	"io"
+
+	"golang.org/x/crypto/chacha20poly1305"
+)
+
+type SymmetricEncryptor struct {
+	in        io.Reader
+	out       io.Writer
+	blockSize int
+	key       [32]byte
+}
+
+func NewSymmetricEncryptor(in io.Reader, out io.Writer, blockSize int) (*SymmetricEncryptor, error) {
+	var key [32]byte
+	_, err := io.ReadFull(rand.Reader, key[:])
+	if err != nil {
+		return nil, err
+	}
+
+	return &SymmetricEncryptor{
+		in:        in,
+		out:       out,
+		blockSize: blockSize,
+		key:       key,
+	}, nil
+}
+
+func (f *SymmetricEncryptor) Encrypt() error {
+	aead, err := chacha20poly1305.New(f.key[:])
+	if err != nil {
+		return err
+	}
+
+	var nonce [12]byte
+	if _, err := io.ReadFull(rand.Reader, nonce[:]); err != nil {
+		return err
+	}
+
+	if err := binary.Write(f.out, binary.LittleEndian, nonce); err != nil {
+		return err
+	}
+
+	buf := make([]byte, f.blockSize)
+	for {
+		n, err := f.in.Read(buf)
+		if err == io.EOF {
+			break
+		}
+		if err != nil {
+			return err
+		}
+
+		encrypted := aead.Seal(nil, nonce[:], buf[:n], nil)
+
+		if _, err := f.out.Write(encrypted); err != nil {
+			return err
+		}
+
+		binary.LittleEndian.PutUint64(nonce[4:], binary.LittleEndian.Uint64(nonce[4:])+1)
+	}
+
+	return nil
+}
